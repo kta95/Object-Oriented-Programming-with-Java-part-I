cf3bea5d070898b6c555add7f0fad5dc83a6dc9b
Kyaw Thet Aung <billionairekta@gmail.com>
2020-10-29 22:07:49 +0630

    week4

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+        Account myAcc = new Account("Kyaw Thet Aung", 100.0);
+        myAcc.deposit(20.0);
+        System.out.println(myAcc);
+    }
+
+}
