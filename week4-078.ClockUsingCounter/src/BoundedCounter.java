d4a2634b6f1ab21122272ed8f61bccd486b42107
Kyaw Thet Aung <billionairekta@gmail.com>
2020-11-03 22:29:05 +0630

    objects

---
+++ week4-078.ClockUsingCounter/src/BoundedCounter.java
@@ -0,0 +1,34 @@
+public class BoundedCounter {
+    private int value;
+    private int upperLimit;
+    
+    public int getValue() {
+        return this.value;
+    }
+    
+    public void setValue( int newValue) {
+        if (newValue <= this.upperLimit && newValue > 0) {
+            this.value = newValue;
+        }
+    }
+    
+    public BoundedCounter(int upperBound) {
+        this.value = 0;
+        this.upperLimit = upperBound;
+    }
+    
+    public void  next() {
+        if (this.value >= this.upperLimit) {
+            this.value = 0;
+        } else {
+            this.value++;
+        }
+    }
+    
+    public String toString() {
+        if (this.value < 10) {
+            return "0" + this.value;
+        }
+        return "" + this.value;
+    }
+}
