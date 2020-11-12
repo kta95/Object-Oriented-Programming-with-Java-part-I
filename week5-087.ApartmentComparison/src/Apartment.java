5e2d175740f3626714f55080ec12eaee17900ad3
Kyaw Thet Aung <billionairekta@gmail.com>
2020-11-12 21:36:04 +0630

    week5

--- week5-087.ApartmentComparison/src/Apartment.java
+++ week5-087.ApartmentComparison/src/Apartment.java
@@ -11,4 +11,29 @@ public class Apartment {
         this.pricePerSquareMeter = pricePerSquareMeter;
     }
     
+    public boolean larger(Apartment otherApartment) {
+        if (this.squareMeters > otherApartment.squareMeters) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int priceDifference(Apartment otherApartment) {
+        int tempValue = this.pricePerSquareMeter * this.squareMeters;
+        int otherValue = otherApartment.pricePerSquareMeter * otherApartment.squareMeters;
+        int result = tempValue - otherValue;
+        if (result < 0) {
+            return result * (-1);
+        }
+        return result;
+    }
+    
+    public boolean moreExpensiveThan(Apartment otherApartment) {
+        int tempValue = this.pricePerSquareMeter * this.squareMeters;
+        int otherValue = otherApartment.pricePerSquareMeter * otherApartment.squareMeters;
+        if (tempValue > otherValue) {
+            return true;
+        }
+        return false;
+    }
 }
