cc0453231e2b2595ae04fdc9ed6004c8ee65cfcf
Kyaw Thet Aung <billionairekta@gmail.com>
2020-11-18 22:36:58 +0630

    week6

--- week6-095.Money/src/Money.java
+++ week6-095.Money/src/Money.java
@@ -33,4 +33,55 @@ public class Money {
         return euros + "." + zero + cents + "e";
     }
 
+    public Money plus(Money added) {
+        int myEuros = this.euros + added.euros();
+        int myCents = this.cents + added.cents();
+        if (myCents > 99) {
+            myEuros += myCents / 100;
+            myCents %= 100;
+        }        
+        Money newMoney = new Money(myEuros, myCents);
+        
+        return newMoney;
+    }
+    
+    public boolean less(Money compared) {
+        if (this.euros > compared.euros()) {
+            return false;
+        } else if (this.euros == compared.euros()) {
+            if (this.cents >= compared.cents()) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+        
+    public Money minus(Money decremented) {
+        int myEuros = this.euros - decremented.euros();
+        int myCents = this.cents;
+        if (myEuros < 0) {
+            myEuros = 0;
+        }
+        if (this.cents < decremented.cents()) {
+            myEuros -= 1;
+            myCents += 100;
+        }
+        myCents = myCents - decremented.cents();
+        if (myCents > 99) {
+            myEuros += myCents / 100;
+            myCents %= 100;
+        }
+        if (myCents < 0) {
+            myCents = 0;
+        }
+        if (this.less(decremented)) {
+            myEuros = 0;
+            myCents = 0;
+        }
+        Money newMoney = new Money(myEuros, myCents);
+        
+        return newMoney;        
+    }
 }
