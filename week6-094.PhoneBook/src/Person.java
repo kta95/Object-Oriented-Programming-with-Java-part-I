26dd6677a9ec7573bb94381863657be269a82120
Kyaw Thet Aung <billionairekta@gmail.com>
2020-11-18 22:00:48 +0630

    week6

---
+++ week6-094.PhoneBook/src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private String name;
+    private String number;
+    
+    public Person(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return name + " number: " + number;
+    }
+    
+    public void changeNumber(String newNumber) {
+        this.number = newNumber;
+    }
+    
+}
