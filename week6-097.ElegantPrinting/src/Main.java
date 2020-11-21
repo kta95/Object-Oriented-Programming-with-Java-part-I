4f017b93c54a2c542265a930ef848bfbc36b6f84
Kyaw Thet Aung <billionairekta@gmail.com>
2020-11-21 22:11:31 +0630

    week6

--- week6-097.ElegantPrinting/src/Main.java
+++ week6-097.ElegantPrinting/src/Main.java
@@ -9,5 +9,12 @@ public class Main {
 
     public static void printElegantly(int[] array) {
         // Write code here
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i + 1 != array.length) {
+                System.out.print(", ");
+            }
+            
+        }
     }
 }
